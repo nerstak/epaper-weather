510b41654636147f7c25c4bca18d5eb92af96677
nerstak <roy.karst@gmail.com>
2023-05-04 20:06:48 +0200

    doc: adding comments on new functions

--- dht22.py
+++ dht22.py
@@ -8,14 +8,25 @@ from metrics_handler import log_local_temperature_humidity
 _pin = board.D4
 
 def _create_connection_sensor() -> adafruit_dht.DHT22:
+    """
+    Connect to sensor
+    :return: Object of DHT22 with connection initialized
+    """
     return adafruit_dht.DHT22(_pin)
 
 dhtDevice = _create_connection_sensor()
 
 def exit_dht_sensor():
+    """
+    Properly exit sensor
+    """
     dhtDevice.exit()
 
 def get_temperature_humidity() -> (float, float):
+    """
+    Get temperature & humidity
+    :return: (temperature, humidity)
+    """
     temp, hum =  retrieve_data_sensor()
     if CONFIG["metrics"]["record_metrics"]:
        log_local_temperature_humidity(temp, hum)
@@ -23,13 +34,21 @@ def get_temperature_humidity() -> (float, float):
 
 
 def retrieve_data_sensor(tries = 5) -> (float, float):
-    try:
-        temp = float(dhtDevice.temperature)
-        hum = float(dhtDevice.humidity)
-    except RuntimeError as error:
-        log.warn(error)
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
-    else:
-        return temp, hum
+    """
+    Retrieve data from sensor
+    :param tries: number of time to accept an error
+    :return:
+    """
+    nb_try = 0
+    while nb_try < tries:
+        try:
+            temp = float(dhtDevice.temperature)
+            hum = float(dhtDevice.humidity)
+        except RuntimeError as error:
+            log.warn(error)
+            nb_try+=1
+        except Exception as error:
+            dhtDevice.exit()
+            raise error
+        else:
+            return temp, hum
