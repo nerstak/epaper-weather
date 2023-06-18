f0d733cb5f18fba7dd90a058830891dd08595d6d
nerstak <roy.karst@gmail.com>
2023-06-18 22:25:06 +0200

    fix: sensor reporting 25.5 as wrong value

--- dht22.py
+++ dht22.py
@@ -1,3 +1,5 @@
+import time
+
 import board
 import adafruit_dht
 
@@ -40,9 +42,15 @@ def retrieve_data_sensor(tries = 5) -> (float, float):
     dhtDevice = _create_connection_sensor()
     nb_try = 0
     while nb_try < tries:
+        time.sleep(1)
         try:
             temp = float(dhtDevice.temperature)
             hum = float(dhtDevice.humidity)
+
+            # Sometimes, the sensor will return 25.5, which is incorrect
+            # See https://forum.arduino.cc/t/arduino-due-dht22-wrong-value/1035449/3
+            if temp == 25.5 and hum == 25.5:
+                raise RuntimeError("Wrong value: 25.5")
         except RuntimeError as error:
             log.warn(error)
             nb_try+=1
